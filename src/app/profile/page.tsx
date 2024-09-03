cc7a55e4229653b5fc061c66f928a34a03b06e22
faroukemad <faroukemad.pro@yahoo.com>
2024-09-03 18:23:35 +0300

    intialize

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Suspense } from "react";
 import { useSearchParams } from 'next/navigation';
 import ProfileSide from "./ProfileSide";
 import { fetchProfile } from './ProfileService';
 import ProfileHead from "./ProfileHead";
 import ProfileTab from "./ProfileTab";
-import './profile.scss'
+import './profile.scss';
 
 interface ProfileData {
     first_name: string;
@@ -19,15 +19,14 @@ interface ProfileData {
     image: string;
 }
 
-export default function Login() {
+function ProfileComponent() {
     const [isOpen, setIsOpen] = useState(false);
     const searchParams = useSearchParams();
-    const handleToggle = () => {setIsOpen(!isOpen);};
+    const handleToggle = () => { setIsOpen(!isOpen); };
     const [profileData, setProfileData] = useState<ProfileData | null>(null);
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
-
         const accessToken = searchParams.get('access');
         if (accessToken) {
             const fetchProfileData = async () => {
@@ -44,34 +43,34 @@ export default function Login() {
             setError('No access token found.');
         }
     }, [searchParams]);
+
     return (
         <div className='w-screen overflow-auto bg-[#F8F8F8] h-screen'>
-
             <div className="flex overflow-hidden p-8">
-
                 <ProfileSide isOpen={isOpen} onToggle={handleToggle} />
                 <div className="w-[70%] flex flex-col px-8">
-
                     <ProfileHead
                         image={profileData?.image}
                         name={profileData?.name}
                         bio={profileData?.bio}
                         email={profileData?.email} />
-
                     <div className="bg-[#A2A1A833] w-full h-[1px] mt-[25px]"></div>
-
                     <div className="w-4/5 mt-3">
-
                         <div className="flex w-full flex-col">
                             <ProfileTab profileData={profileData} />
                         </div>
-
-                        
                     </div>
                 </div>
             </div>
-
             {error && (<div className="text-red-500 text-center absolute top-0">{error}</div>)}
         </div >
-    )
+    );
+}
+
+export default function Login() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <ProfileComponent />
+        </Suspense>
+    );
 }
